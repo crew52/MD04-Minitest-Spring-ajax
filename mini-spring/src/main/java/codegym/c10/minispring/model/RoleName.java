0b558e7e96a4bd7f4052f039fda68ab7408217b2
crew52 <dungnc150602@gmail.com>
2025-03-13 10:10:12 +0700

    add model

---
+++ mini-spring/src/main/java/codegym/c10/minispring/model/RoleName.java
@@ -0,0 +1,5 @@
+package codegym.c10.minispring.model;
+
+public enum RoleName {
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ mini-spring/src/main/java/codegym/c10/minispring/model/User.java
@@ -0,0 +1,21 @@
+package codegym.c10.minispring.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+}
